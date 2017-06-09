fcbaa42347907f7720b7f382ed6e6c64e2e55ce3
Lanka, Atchutaram <alanka@tva.gov>
2017-06-09 15:58:47 -0400

    Worked on Login and Register Components

--- PlanetSupplements/src/app/register/register.component.ts
+++ PlanetSupplements/src/app/register/register.component.ts
@@ -12,10 +12,43 @@ export class RegisterComponent implements OnInit {
     { value: 'TN', viewValue: 'Tennessee' },
     { value: 'VA', viewValue: 'Virginia' }
   ];
-  
+
+  email: string;
+
+  password: string;
+
+  confirmPassword: string;
+
+  firstName: string;
+
+  lastName: string;
+
+  phoneNumber: string;
+
+  address1: string;
+
+  address2: string;
+
+  city: string;
+
+  state: string;
+
+  zipCode: string;
+
   constructor() { }
 
   ngOnInit() {
+  
   }
 
+  registerUser() {
+    alert("Email required");
+  }
+
+  clearForm() {
+    this.email = this.password = this.confirmPassword
+      = this.firstName = this.lastName = this.phoneNumber
+      = this.address1 = this.address2 = this.city
+      = this.zipCode = this.state = null;
+  }
 }
