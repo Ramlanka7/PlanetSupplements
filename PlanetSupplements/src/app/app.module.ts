3a43e3ae100c26b4bda865322d9eca9da2993c9a
Lanka, Atchutaram <alanka@tva.gov>
2017-06-09 09:36:15 -0400

    Added Routing and login components

--- PlanetSupplements/src/app/app.module.ts
+++ PlanetSupplements/src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(routes, { useHash: false }),
     BrowserModule
   ],
   providers: [],
