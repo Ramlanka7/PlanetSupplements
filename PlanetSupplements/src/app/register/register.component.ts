95cd7d3af32b854fb440784b96e36e682bc179ae
Lanka, Atchutaram <alanka@tva.gov>
2017-06-13 16:42:04 -0400

    Validations for Register

--- PlanetSupplements/src/app/register/register.component.ts
+++ PlanetSupplements/src/app/register/register.component.ts
@@ -7,7 +7,6 @@ import { Register } from 'app/Model/Register';
   styleUrls: ['./register.component.less']
 })
 export class RegisterComponent {
-
   private register = new Register();
 
   invalid = false;
@@ -23,8 +22,13 @@ export class RegisterComponent {
   }
 
   registerUser() {
-    this.invalid = true;
-    this.errors.push("Email is required.");
+    this.errors = [];
+    this.validateModel();
+    if (this.errors.length > 0) {
+      this.invalid = true;
+    } else {
+      //todo: Call web api to register user
+    }
   }
 
   clearForm() {
@@ -45,5 +49,46 @@ export class RegisterComponent {
       // invalid character, prevent input
       event.preventDefault();
     }
+  }
+
+  validateModel(): any {
+    //validate Email
+    if (this.register.email) {
+      if (!this.validateEmail(this.register.email)) {
+        this.errors.push("Please enter valid Email.");
+      }
+    } else {
+      this.errors.push("Email is required.");
+    }
+
+    //validate Password
+    if (this.register.password && this.register.confirmPassword) {
+      if (this.register.password !== this.register.confirmPassword) {
+        this.errors.push("Password and Confirm Password are not same.");
+      }
+    }
+    else {
+      if (!this.register.password) {
+        this.errors.push("Password is required.");
+      }
+      if (!this.register.confirmPassword) {
+        this.errors.push("Confirm Password is required.");
+      }
+    }
+
+    //validate First Name
+    if (!this.register.firstName) {
+      this.errors.push("First Name is required.");
+    }
+
+    //validate Last Name
+    if (!this.register.lastName) {
+      this.errors.push("Last Name is required.");
+    }
+  }
+
+  validateEmail(email: string) {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(email);
   }
 }
