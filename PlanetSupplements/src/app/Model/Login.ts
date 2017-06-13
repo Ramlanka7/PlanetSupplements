4e01a44be772b966d18b4673afdf1f64f6557b43
Lanka, Atchutaram <alanka@tva.gov>
2017-06-13 10:20:34 -0400

    Worked on Login component

---
+++ PlanetSupplements/src/app/Model/Login.ts
@@ -0,0 +1,7 @@
+﻿export class Login {
+  email: string;
+
+  password: string;
+
+  rememberMe: boolean;
+}
--- PlanetSupplements/src/app/login/login.component.ts
+++ PlanetSupplements/src/app/login/login.component.ts
@@ -1,13 +1,28 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component } from '@angular/core';
+import { Login } from 'app/Model/Login';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less']
 })
-export class LoginComponent implements OnInit {
-  constructor() { }
+export class LoginComponent {
+  private login = new Login();
 
-  ngOnInit() {
+  invalid = false;
+  canUserLogin() {
+    if (this.login.email && this.login.password) {
+      this.invalid = this.validateEmail(this.login.email);
+    } else {
+      this.invalid = false;
+    }
+  }
+
+  loginUser() {
+  }
+
+  validateEmail(email: string) {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(email);
   }
 }
