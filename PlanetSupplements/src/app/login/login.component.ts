346878c706496ef7775a4d45ba9bc6bf892dfc5f
Ram Lanka <Ramlanka@outlook.com>
2019-03-11 21:15:55 -0400

    added back

--- PlanetSupplements/src/app/login/login.component.ts
+++ PlanetSupplements/src/app/login/login.component.ts
@@ -32,6 +32,7 @@ export class LoginComponent {
   }
 
   validateEmail(email: string) {
-   return true;
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(email);
   }
 }
