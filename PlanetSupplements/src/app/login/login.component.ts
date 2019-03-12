2e8dd0e7803f85a5dcee74e2f6c826de5b7a150b
Ram Lanka <Ramlanka@outlook.com>
2019-03-11 21:08:44 -0400

    removed login

--- PlanetSupplements/src/app/login/login.component.ts
+++ PlanetSupplements/src/app/login/login.component.ts
@@ -28,11 +28,10 @@ export class LoginComponent {
   }
 
   loginUser() {
-    return this.http.
+    //return this.http.
   }
 
   validateEmail(email: string) {
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return emailRegex.test(email);
+   return true;
   }
 }
